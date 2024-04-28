1b09635462a62caac2cda1bbea68d0f80d26f1d3
Mostafa Tamer <mostafaghobashytamer@gmail.com>
2024-04-28 22:10:49 +0300

    making the sql code (not done)

--- restfull/api.py
+++ restfull/api.py
@@ -1,12 +1,17 @@
 from flask import Flask, jsonify
 import sqlite3
 
+conn = sqlite3.connect(r"restfull\data.db")
+cursor = conn.cursor()
+
+# cursor.execute('''CREATE TABLE IF NOT EXISTS users
+#                   (id INTEGER PRIMARY KEY, name TEXT, email TEXT)''')
 
 app = Flask(__name__)
 
-@app.route('/post/<string:post_id>', methods=["POST"])
+@app.route("/post/<string:post_id>", methods=["POST"])
 def test_post(post_id):
     return jsonify({"message": f"This is a POST request to /post/{post_id}"})
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
