d262422f6312209ca7118be64f2ca5f71b4d4240
Mostafa Tamer <mostafaghobashytamer@gmail.com>
2024-04-28 21:53:44 +0300

    made the first post  response

--- restfull/api.py
+++ restfull/api.py
@@ -1,4 +1,12 @@
-from flask import Flask, jsoify
+from flask import Flask, jsonify
 import sqlite3
 
 
+app = Flask(__name__)
+
+@app.route('/post/<int:post_id>', methods=["POST"])
+def test_post(post_id):
+    return jsonify({"message": f"This is a POST request to /post/{post_id}"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
